386025f8346c5d251432912945575f089e073954
selfservice-test <vishalthakurtest1712@gmail.com>
2020-04-03 11:07:25 +0000

    second commit

---
+++ PrintNumbers.java
@@ -0,0 +1,14 @@
+public class PrintNumbers 
+{
+	public static void main(String[] args)
+	{
+		//print the result
+		System.out.println("Output is : ");
+
+		//loop  to print 1 to 10.
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
